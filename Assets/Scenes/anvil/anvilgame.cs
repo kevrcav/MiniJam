20281e461761e9f1accb49ef905c1c5b7665dadf
BluShine <umbreon27@gmail.com>
2015-09-08 15:49:48 -0400

    Added anvil game.
    Added a game about a falling anvil.

---
+++ Assets/Scenes/anvil/anvilgame.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class anvilgame : MonoBehaviour {
+
+    public float speed = 100;
+
+    bool onGround = false;
+    public pianohit piano;
+
+    float gameTimer = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameTimer += Time.deltaTime;
+        if (gameTimer >= 4 && !onGround)
+        {
+            onGround = true;
+            if (piano.hit)
+            {
+                MiniGame.Instance.ReportWin();
+            }
+            else
+            {
+                MiniGame.Instance.ReportLose();
+            }
+        }
+
+        if (!onGround)
+        {
+            transform.position += new Vector3(speed * Time.deltaTime * Input.GetAxis("Horizontal"), 0, 0);
+        }
+	}
+}
---
+++ Assets/Scenes/anvil/pianohit.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pianohit : MonoBehaviour {
+
+    public bool hit = false;
+    public SpriteRenderer crush;
+
+	// Use this for initialization
+	void Start () {
+        if (Random.value < .5f)
+        {
+            transform.position = new Vector3(-transform.position.x, transform.position.y, transform.position.z);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (!hit)
+        {
+            hit = true;
+            GetComponent<SpriteRenderer>().enabled = false;
+            crush.enabled = true;
+        }
+    }
+}
