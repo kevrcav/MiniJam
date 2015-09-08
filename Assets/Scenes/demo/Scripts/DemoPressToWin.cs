06fa91896e36ae2ce81ebcbb6325f7d508c6aa13
BluShine <umbreon27@gmail.com>
2015-09-08 11:51:40 -0400

    Made the demo winnable.
    Fixed the demo mingame so that it is actually winnable. Also made the
    dot bigger.

--- Assets/Scenes/demo/Scripts/DemoPressToWin.cs
+++ Assets/Scenes/demo/Scripts/DemoPressToWin.cs
@@ -3,15 +3,14 @@ using System.Collections;
 
 public class DemoPressToWin : MonoBehaviour {
 
-   bool won;
    public SpriteRenderer sprite;
 
 	void Update()
    {
-      if (!won && Input.GetAxis("Button 1") > 0)
+      if (Input.GetAxis("Button 1") > 0)
       {
-         MiniGame.Instance.ReportWin();
          sprite.color = new Color(0, 1, 0);
+         MiniGame.Instance.ReportWin();
       }
    }
 }
