66e4e10d6c0cbcf800421d565eae8d8af3c2c924
Liam <lcheeseboy@gmail.com>
2015-09-08 14:25:19 -0400

    Tree stealth game

---
+++ Assets/Scenes/tree_stealth/Bystander.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Bystander : MonoBehaviour {
+
+    float tick1;
+    int tick2;
+    float startX, startY;
+    float xOffset;
+    bool tickUp = false;
+
+    public List<int> pauseTicks;
+    public float moveSpeed = 0.5f, bounceSpeed = 3, bounceHeight = 1;
+
+    bool gameOvered = false;
+
+	// Use this for initialization
+	void Start () {
+        tick1 = 0;
+        tick2 = 0;
+        startX = transform.position.x;
+        startY = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        tick1 += Time.deltaTime;
+        float sinVal = Mathf.Abs(Mathf.Sin(bounceSpeed*tick1));
+        transform.position = new Vector3(startX + xOffset, startY + bounceHeight*sinVal, 0);
+        if(sinVal < 0.05f && !tickUp)
+        {
+            tick2++;
+            tickUp = true;
+        }
+        if(sinVal > 0.95f)
+        {
+            tickUp = false;
+        }
+        if(!pauseTicks.Contains(tick2) && !FallingTree.gameOver)
+        {
+            xOffset += moveSpeed * Time.deltaTime;
+        }
+        if(FallingTree.gameOver && !gameOvered)
+        {
+            transform.localScale = new Vector2(transform.localScale.x * -1, 1);
+            gameOvered = true;
+        }
+
+	}
+
+    public bool isOffscreen()
+    {
+        float edge = Camera.main.orthographicSize * 4f / 3f;
+        float xExts = GetComponent<SpriteRenderer>().bounds.extents.x;
+        float xPos = transform.position.x;
+        return (xPos > edge + xExts || xPos < -(edge + xExts));
+    }
+}
---
+++ Assets/Scenes/tree_stealth/FallingTree.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingTree : MonoBehaviour {
+
+    Bystanders bys;
+    public static bool gameOver;
+
+    public float fallSpeed = 3f, pushForce = 3f;
+    public SpriteRenderer face;
+    public Sprite smug, spooked, dizzy;
+
+	// Use this for initialization
+	void Start () {
+        bys = FindObjectOfType<Bystanders>();
+        gameOver = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float zAngle = transform.eulerAngles.z;
+        if (zAngle > 180) zAngle = zAngle - 360;
+        if(zAngle < 30 && zAngle > -30)
+        {
+            face.sprite = smug;
+        }
+        else if (zAngle > 90 || zAngle < -90)
+        {
+            if (gameOver) return;
+            if (bys.allBystandersOffscreen())
+            {
+                transform.parent.GetComponent<MiniGame>().ReportWin();
+                Debug.Log("win");
+            }
+            else
+            {
+                transform.parent.GetComponent<MiniGame>().ReportLose();
+                Debug.Log("lose");
+            }
+            face.sprite = dizzy;
+            gameOver = true;
+            return;
+        }
+        else
+        {
+            face.sprite = spooked;
+        }
+        float hInput = Input.GetAxis("Horizontal");
+        transform.eulerAngles = new Vector3(0, 0, zAngle + Time.deltaTime*(fallSpeed * zForce(zAngle) + pushForce*-100*hInput));
+	}
+
+    float zForce(float zAngle)
+    {
+        return (zAngle > 0) ? Mathf.Max(zAngle, 20) : Mathf.Min(zAngle, -20);
+    }
+}
