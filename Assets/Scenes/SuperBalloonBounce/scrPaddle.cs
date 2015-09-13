8b2bae0a22bd9040d3618b2dde2bca60b1a4f20e
Michael Rinaldi <mikerinaldi1@yahoo.com>
2015-09-13 13:47:18 -0400

    Super Balloon Bounce
    By Michael Rinaldi, Graham Ritter, and Oakley Reid
    
    Move and Rotate the paddle with your mouse. Don't let the balloon touch
    the spikes!

---
+++ Assets/Scenes/SuperBalloonBounce/scrPaddle.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scrPaddle : MonoBehaviour {
+
+	Vector3 lastMove = new Vector3(0, 0, 0);
+	Rigidbody rb;
+	public GameObject ourCam;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.velocity = Vector3.zero;
+		Vector3 mousePos = ourCam.GetComponent<Camera>().ScreenToWorldPoint (Input.mousePosition);
+		mousePos = new Vector3 (Mathf.Max (Mathf.Min (10, mousePos.x), -10), Mathf.Max (Mathf.Min (-2, mousePos.y), -6), 0);
+		lastMove = mousePos - transform.position;
+		rb.velocity = lastMove * 20;
+		//transform.position = new Vector3 (mousePos.x, mousePos.y, 0);
+
+		//left mouse button
+		if (Input.GetMouseButton (0)) {
+			transform.Rotate(new Vector3 (0, 0, 2));
+		} 
+		//right mouse button
+		else if (Input.GetMouseButton (1)) {
+			transform.Rotate(new Vector3 (0, 0, -2));
+		}
+	}
+
+	public Vector3 getLastMove() { return lastMove;}
+}
