f3c1fd176388e06b4eca45ba6c0c01b65c01b6a4
Kevin Cavanaugh <kevrcav@gmail.com>
2015-09-13 14:37:24 -0400

    adding Praise Jeebus

---
+++ Assets/Scenes/praisejeebus/JeebusWorshipper.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class JeebusWorshipper : MonoBehaviour {
+
+    public Sprite praisingSprite;
+    public GameObject arrow;
+    SpriteRenderer spriteRenderer;
+
+    public enum Direction
+    {
+        kUp,
+        kRight,
+        kDown,
+        kLeft
+    }
+
+    Direction direction;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        direction = (Direction)Random.Range(0, 4);
+        arrow.transform.rotation = Quaternion.AngleAxis(90.0f * (int)direction, Vector3.back);
+    }
+
+	public bool CheckPrayer () {
+
+        switch (direction)
+        {
+            case Direction.kUp:
+                if (Input.GetAxis("Vertical") > 0)
+                {
+                    PrayerAnswered();
+                    return true;
+                }
+                break;
+            case Direction.kDown:
+                if (Input.GetAxis("Vertical") < 0)
+                {
+                    PrayerAnswered();
+                    return true;
+                }
+                break;
+            case Direction.kLeft:
+                if (Input.GetAxis("Horizontal") < 0)
+                {
+                    PrayerAnswered();
+                    return true;
+                }
+                break;
+            case Direction.kRight:
+                if (Input.GetAxis("Horizontal") > 0)
+                {
+                    PrayerAnswered();
+                    return true;
+                }
+                break;
+        }
+
+        return false;
+    }
+
+    void PrayerAnswered()
+    {
+        spriteRenderer.sprite = praisingSprite;
+    }
+}
---
+++ Assets/Scenes/praisejeebus/JeebusWorshipperMgr.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class JeebusWorshipperMgr : MonoBehaviour {
+
+    public int numberWorshippers;
+    public Transform worshipperStart;
+    public JeebusWorshipper worshipperPrefab;
+
+    List<JeebusWorshipper> worshippers = new List<JeebusWorshipper>();
+    int currentWorshipper = 0;
+
+    bool ready = true;
+
+    void Start()
+    {
+        Vector3 currentSpawnPos = worshipperStart.position;
+        for (int i = 0; i < numberWorshippers; ++i)
+        {
+            JeebusWorshipper newWorshipper = (JeebusWorshipper)Instantiate(worshipperPrefab, currentSpawnPos, Quaternion.identity);
+            worshippers.Add(newWorshipper);
+            newWorshipper.transform.parent = transform;
+            currentSpawnPos.x += 1.9f;
+        }
+    }
+
+    void Update()
+    {
+
+        if (currentWorshipper >= worshippers.Count) return;
+
+        if (!ready)
+        {
+            ready = Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") == 0;
+            return;
+        }
+        if (worshippers[currentWorshipper].CheckPrayer())
+        {
+            if (++currentWorshipper >= worshippers.Count)
+                MiniGame.Instance.ReportWin();
+            ready = false;
+        }
+	}
+}
